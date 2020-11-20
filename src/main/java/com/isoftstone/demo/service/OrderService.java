4b5b9c8c42f5458db38163cfc3d0a9463755b1c5
Xavier Bai <tocreationbai@gmail.com>
2020-11-20 21:02:53 +0800

    完成更新状态接口

--- src/main/java/com/isoftstone/demo/service/OrderService.java
+++ src/main/java/com/isoftstone/demo/service/OrderService.java
@@ -1,4 +1,11 @@
 package com.isoftstone.demo.service;
 
+import com.isoftstone.demo.common.exception.OrderStatusException;
+import com.isoftstone.demo.pojo.Order;
+
+
 public interface OrderService {
+    Order findById(int id);
+
+    void updateStatus(int statusCode, int id) throws OrderStatusException;
 }
